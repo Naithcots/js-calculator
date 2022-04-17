16e8d7788ec1b3ae35dc1daba8dfa359e447f80b
Naithcots <sebastian.stochmial2000@gmail.com>
2022-04-17 12:45:55 +0200

    Update logic and display values

--- app.js
+++ app.js
@@ -4,22 +4,78 @@ const history = document.querySelector(".history");
 
 let a = "";
 let b = "";
-let operator = null;
-let historyStr = "";
+let operator = "";
+let displayValue = "";
+let recentValue = "";
 
-display.textContent = 0;
+const isFloat = (num) => num.includes(".");
 
-const generateHistory = (fin) => {
-  historyStr = "";
-  if (fin) {
-    historyStr = a + operator + b + "=";
-    history.textContent = historyStr;
+const updateUI = () => {
+  displayValue
+    ? (display.textContent = displayValue)
+    : (display.textContent = 0);
+  history.textContent = recentValue;
+};
+
+const evaluate = (e) => {
+  const isOperator = e.target.classList.contains("operator");
+  const value = e.target.textContent;
+
+  if (value === "AC") {
+    clear();
     return;
   }
-  if (a) historyStr += a;
-  if (operator) historyStr += operator;
-  if (b) historyStr += b;
-  history.textContent = historyStr;
+
+  if (a && b && operator && value === "=") {
+    recentValue = `${a}${operator}${b}=`;
+    a = operate(a, operator, b);
+    displayValue = a;
+    b = "";
+    updateUI();
+    return;
+  }
+
+  if (!a && isOperator) return;
+
+  if (!isOperator) {
+    if (!operator) {
+      if (value === "." && isFloat(a)) return;
+      if (a === "0" && value === "0") {
+        a = "0";
+        return;
+      }
+      if (!a && value === ".") {
+        a = "0";
+      }
+      a += value;
+    } else {
+      if (value === "." && isFloat(b)) return;
+      if (b === "0" && value === "0") {
+        b = "0";
+        return;
+      }
+      if (!b && value === ".") {
+        b = "0";
+      }
+      b += value;
+    }
+  } else if (isOperator) {
+    if (!operator) {
+      operator = value;
+    } else if (!b && operator) {
+      operator = value;
+    } else if (a && b && operator) {
+      recentValue = `${a}${operator}${b}=`;
+      a = operate(a, operator, b);
+      displayValue = a;
+      b = "";
+      operator = value;
+      updateUI();
+    }
+  }
+  displayValue = `${a}${operator}${b}`;
+  updateUI();
+  // console.log({ a, operator, b }, value);
 };
 
 const add = (a, b) => {
@@ -38,66 +94,38 @@ const divide = (a, b) => {
   return a / b;
 };
 
-const operate = (operator, a, b) => {
+const operate = (a, operator, b) => {
   let result;
-  if (operator === "+") result = add(a, b);
-  if (operator === "-") result = subtract(a, b);
-  if (operator === "×") result = multiply(a, b);
-  if (operator === "÷") result = divide(a, b);
-  return Math.round(result * 10000) / 10000;
+  switch (operator) {
+    case "+":
+      result = add(+a, +b);
+      return Math.round(result * 100) / 100;
+    case "-":
+      result = subtract(+a, +b);
+      return Math.round(result * 100) / 100;
+
+    case "×":
+      result = multiply(+a, +b);
+      return Math.round(result * 100) / 100;
+
+    case "÷":
+      if (b === "0") return "ERR";
+      result = divide(+a, +b);
+      return Math.round(result * 100) / 100;
+  }
 };
 
-const reset = () => {
+const clear = () => {
   a = "";
   b = "";
-  operator = null;
-  history.textContent = "";
-  display.textContent = "0";
+  operator = "";
+  displayValue = "";
+  recentValue = "";
+  updateUI();
 };
 
+display.textContent = 0;
+
 [...buttons.children].forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const isOperator = e.target.classList.contains("operator") ? true : false;
-    const value = e.target.textContent;
-    // console.log(value);
-
-    if (value === "AC") {
-      reset();
-      return;
-    }
-
-    if (a && b && value === "=") {
-      console.log("calculating!");
-      generateHistory("=");
-      a = operate(operator, +a, +b);
-      display.textContent = a;
-      b = "";
-      return;
-    } else if (value === "=") return;
-
-    if (a && b && isOperator) {
-      a = operate(operator, +a, +b);
-      display.textContent = a;
-      b = "";
-      operator = value;
-      return;
-    } else if (isOperator) {
-      operator = value;
-      generateHistory();
-      return;
-    }
-
-    if (!isOperator) {
-      if (value === "." && display.textContent.includes(".")) return;
-      if (!operator) {
-        a += value;
-        display.textContent = a;
-      } else {
-        b += value;
-        display.textContent = b;
-      }
-    }
-
-    generateHistory();
-  });
+  button.addEventListener("click", evaluate);
 });
